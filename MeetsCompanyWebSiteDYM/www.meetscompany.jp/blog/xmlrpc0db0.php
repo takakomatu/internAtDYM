9b32e52f50bd2a101fd0170bd39048fe62cfebdf
takakomatu <31825350+takakomatu@users.noreply.github.com>
2018-04-27 02:33:08 -0500

    Add files via upload

---
+++ MeetsCompanyWebSiteDYM/www.meetscompany.jp/blog/xmlrpc0db0.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>https://wordpress.org/</engineLink>
+    <homePageLink>https://www.meetscompany.jp/blog</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="https://www.meetscompany.jp/blog/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://www.meetscompany.jp/blog/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://www.meetscompany.jp/blog/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="https://www.meetscompany.jp/blog/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="https://www.meetscompany.jp/blog/index.php?rest_route=/" />
+	    </apis>
+  </service>
+</rsd>
